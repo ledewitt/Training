b1c4bbb385b7a5d0f6721e3c93b6ab9f64b67e64
Luke DeWitt <luke@dewittsoft.com>
2015-04-03 14:35:03 -0600

    More snippets from p 124.

--- Well_Grounded_Rubyist/chp5/p_124_Class_Rigging.rb
+++ Well_Grounded_Rubyist/chp5/p_124_Class_Rigging.rb
@@ -7,3 +7,12 @@ end
 c = C.new
 c.x
 puts "That was a call to x by: #{c}"
+
+puts "--- Next Snippet on Page. ---"
+
+obj = Object.new
+def obj.show_me
+  puts "Inside singleton method show_me of #{self}"
+end
+obj.show_me
+puts "Back from call to show_me by #{obj}"
