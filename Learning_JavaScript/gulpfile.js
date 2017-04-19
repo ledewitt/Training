3635bffd86077acdfe441fbdc321932f08fde76a
Luke DeWitt <luke@dewittsoft.com>
2017-04-19 14:43:09 -0600

    Finishing up the tools in Chapter 2 work.

--- Learning_JavaScript/gulpfile.js
+++ Learning_JavaScript/gulpfile.js
@@ -1,9 +1,14 @@
 // Gulp depndencies go here
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
   // Gulp tasks go here
+  // Run ESLint
+  gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format());
   // Node source
   gulp.src("es6/**/*.js")
     .pipe(babel())
