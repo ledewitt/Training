ad010d6dc5d6ea814a1b6ef385d313091d3317f3
Luke DeWitt <luke@dewittsoft.com>
2017-04-19 14:17:10 -0600

    Adding the ability to Transcompile es6 to es5.

---
+++ Learning_JavaScript/gulpfile.js
@@ -0,0 +1,15 @@
+// Gulp depndencies go here
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('default', function() {
+  // Gulp tasks go here
+  // Node source
+  gulp.src("es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+  // Browser source
+  gulp.src("public/es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
+});
